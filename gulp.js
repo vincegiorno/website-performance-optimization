d5fb7b8e4473f40e7e9972915dea5ebfeb45b62e
vinceg <vincegiorno324@gmail.com>
2015-03-02 00:01:39 -0500

    Add gulp.js

---
+++ gulp.js
@@ -0,0 +1,8 @@
+var gulp = require('gulp'),
+    gulpLoadPlugins = require('gulp-load-plugins'),
+    aux = gulpLoadPlugins();
+
+    gulp.task('js', function () {
+    	gulp.src('src/**/*.js')
+    	.pipe
+    })
