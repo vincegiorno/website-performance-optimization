14f804fb348e15534f69e7c64d735b46dc0e25b2
vinceg <vincegiorno324@gmail.com>
2015-03-02 22:23:27 -0500

    Finish gulp.js, update package.json

--- gulp.js
+++ gulp.js
@@ -2,7 +2,26 @@ var gulp = require('gulp'),
     gulpLoadPlugins = require('gulp-load-plugins'),
     aux = gulpLoadPlugins();
 
-    gulp.task('js', function () {
-    	gulp.src('src/**/*.js')
-    	.pipe
-    })
+gulp.task('js', function() {
+    return gulp.src('src/**/*.js')
+    .pipe(aux.cached('jsfiles'))
+    .pipe(aux.uglify())
+    .pipe(gulp.dest('.'));
+});
+
+gulp.task('img', function() {
+	return gulp.src(['./**/*.jpg', './**/*.jpeg', './**/*.png', './**/*.gif'])
+	.pipe(aux.cached('imagefiles'))
+	.pipe(aux.imagemin())
+	.pipe(gulp.dest('.'));
+});
+
+gulp.task('css', function() {
+	return gulp.src('./**/*.css')
+	.pipe(aux.cached('cssfiles'))
+	.pipe(aux.uncss())
+	.pipe(aux.minifyCss())
+	.pipe(gulp.dest('.'));
+});
+
+gulp.task('default', ['js', 'img', 'css']);
